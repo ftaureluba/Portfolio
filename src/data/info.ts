2fe6317d60d306f7ad32e082f575f9f3b1666b06
feli <ftaurel@fi.uba.ar>
2024-01-16 18:24:11 -0300

    primer commit?

---
+++ src/data/info.ts
@@ -0,0 +1,56 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Felipe Taurel",
+  jobDescription: "Web Developer",
+  about: `Soy un estudiante de licenciatura en sistemas viviendo en Buenos Aires, Argentina
+   Cuento con 3 años de experiencia en HTML, CSS, SCSS, JavaScript, Bootstrap, Tailwind CSS, StyledComponent, React JS y Next JS.
+   Por lo tanto, he completado con éxito varios proyectos web desde cero. A pesar de ello, aprendo de manera continua
+   y nunca dejo de explorar nuevas tecnologías.
+
+   Así, he adquirido conocimientos de Node JS, Express JS, MongoDB, Python, Firebase y React Native con el tiempo.
+  `,
+
+  experience: [
+    {
+      name: "InterNative Labs, LLC",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "Apr 2022",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+  ],
+
+  education: [
+    {
+      name: "Licenciatura en analisis de sistemas.",
+      location: "Universidad de Buenos Aires.",
+      startDate: "2021",
+      endDate: "presente",
+      description: [
+        "",
+      ],
+    }
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/uzzii-21",
+    email: "mailto:uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+
+  projects: [
+    {
+      title: "E-commerce",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/felitaurel/react",
+      liveUrl: "https://ecommerce-taurel.vercel.app/",
+    },
+  ],
+};
